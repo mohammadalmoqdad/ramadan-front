9c0b7939995e2e3ccd6a23c40c8fc689a94c84fa
AnasAlqady <anasalqady9@gmail.com>
2022-02-19 11:27:09 +0200

    add cratirias

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import AuthProvider from "./contexts/AdminContext";
 import Home from "./components/Home";
 import cookie from "react-cookies";
 import { useState, useEffect } from "react";
+import AddCriterias from "./components/AddCriterias";
 function App() {
   const [test, isTest] = useState(false);
   return (
     <AuthProvider>
+      <AddCriterias />
       <Login />
       <Home test={test} />
       <button
