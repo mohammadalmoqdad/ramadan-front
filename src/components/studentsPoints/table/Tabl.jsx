f682cc036837c21058f1814abc171029a1109d89
momokdad-op <Mohammad.Almokdad@cds-global.com>
2022-03-12 17:56:11 +0300

    working on result table

--- src/components/studentsPoints/table/Tabl.jsx
+++ src/components/studentsPoints/table/Tabl.jsx
@@ -5,13 +5,9 @@ import { dataStudentPoints } from "../../../services/studentsServices";
 import Modals from './modal/Modals'
 
 export default function Tabl(props) {
-  const [showdays, setshowdays] = useState('none')
+  const [showdays, setshowdays] = useState('none');
   const [usaruame, setUsarName] = useState(null);
-
   const [StudentsPoints, setStudentsPoints] = useState(null);
-
-
-
   const dayOfRamdan = (e) => {
     if (showdays === "none") {
       setshowdays("block");
@@ -36,76 +32,16 @@ export default function Tabl(props) {
         console.log("ERROR: " + JSON.stringify(err));
       }
     );
-
   }, [props.usarName || usaruame]);
 
-  // console.log(data);
+  const [studentPoints, setStudentPoints] = useState({}); // re-assign to this variable when reselecting the student info that cones from the props.
+
 
   return (
     <Div>
-      <DivIn>
-        <Table responsive>
-          <thead>
-            <tr>
-              <th>#</th>
-              {Array.from({ length: 5 }).map((_, index) => (
-                <th key={index}>المعايير من اسامة</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              {" "}
-              {/* style={{ display: addGroup }} */}
-              <td>1</td>
-              {Array.from({ length: 5 }).map((_, index) => (
-                <td key={index}>
-                  Table cell {index}
-                  <br />
-                  <input type="text" placeholder="Table cell 2323" />
-                </td>
-              ))}
-              <td>
-                <InputSubmit type="submit" value="" onClick={dayOfRamdan}>
-                  تعديل
-                </InputSubmit>
-              </td>
-              {/* <DivTxtField style={{ display: showdays }} >
-                <Span />
-                <FormInput placeholder='ادخل رقم' type="number" required />
-              </DivTxtField> */}
-              <td>x</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              {Array.from({ length: 5 }).map((_, index) => (
-                <td key={index}>Table cell {index}</td>
-              ))}
-              <td>
-                <InputSubmit type="submit" value="" onClick={dayOfRamdan}>
-                  تعديل
-                </InputSubmit>
-              </td>
-              <td>x</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              {Array.from({ length: 5 }).map((_, index) => (
-                <td key={index}>Table cell {index}</td>
-              ))}
-              <td>
-                <InputSubmit type="submit" value="" onClick={dayOfRamdan}>
-                  تعديل
-                </InputSubmit>
-              </td>
-              <td>x</td>
-            </tr>
-          </tbody>
-        </Table>
-        <td style={{ display: showdays }}>
-          <Modals></Modals>
-        </td>
-      </DivIn>
+
+
+
     </Div>
   );
               }
