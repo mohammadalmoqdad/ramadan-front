f6491ee00dc03fa9d9ce440a67c5df9dd66f5659
habusnaineh <abuomar.abusnaineh@gamil.com>
2022-03-14 22:16:32 +0300

    Fixing conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import "./App.css";
-import AuthProvider, {} from "./contexts/AdminContext";
-import React from "react";
-import AppBrowserRouter from "./components/AppBrowserRouter/AppBrowserRouter";rRouter from "./components/AppBrowserRouter/AppBrowserRouter";
+import AuthProvider from "./contexts/AdminContext";
+import AppBrowserRouter from "./components/AppBrowserRouter/AppBrowserRouter";
 function App() {
   // const [test, isTest] = useState(false);
   return (
