98034cd7fa60aac3c0b51906f3ed62a9fd385c98
mohammadalmoqdad <almekdad.mohammad@gmail.com>
2022-04-04 04:00:03 +0300

    updating the style of the chart

--- src/components/studentsPoints/StudentsPoints.styles.js
+++ src/components/studentsPoints/StudentsPoints.styles.js
@@ -1,95 +1,91 @@
 import styled from "styled-components";
 
 export default styled.div`
-display: flex;
-bottom: 2rem;
-position: relative;
-background: linear-gradient(120deg, #2980b9, #2980b9);
-padding:0;
-margin:0;
-box-sizing:border-box;
+  display: flex;
+  bottom: 2rem;
+  position: relative;
+  background: linear-gradient(120deg, #2980b9, #2980b9);
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 `;
 
 export const PointShow = styled.div`
- width: 100%;
+  width: 100%;
   input:focus {
-     /* border-inline-color: 1px solid lightgray; */
-     margin: 0;
-     padding: 0;
-     outline: none;
-     border: none;
-     background-color: #f4b069;
-     color: #FFFFFF;
- }
+    /* border-inline-color: 1px solid lightgray; */
+    margin: 0;
+    padding: 0;
+    outline: none;
+    border: none;
+    background-color: #f4b069;
+    color: #ffffff;
+  }
 
- input {
-     width: 100%;
- }
-/* height: 70rem; */
-background: linear-gradient(120deg, #2980b9, #2980b9);
-/* justify-content: center; */
- /* align-items: center; */
+  input {
+    width: 100%;
+  }
+  /* height: 70rem; */
+  background: linear-gradient(120deg, #2980b9, #2980b9);
+  /* justify-content: center; */
+  /* align-items: center; */
 
- display: flex;
-  @media (max-width:500px) {
-  margin: auto;
-  /* margin-top: 1rem; */
-  /* width: 25rem; */
-  /* height: 115rem; */
-}
+  display: flex;
+  @media (max-width: 500px) {
+    margin: auto;
+    /* margin-top: 1rem; */
+    /* width: 25rem; */
+    /* height: 115rem; */
+  }
 `;
 
 export const LoginForm = styled.div`
-/* height: 100%; */
-width: 100%;
-background: white;
-padding: 3rem;
-text-align:center;
- /* justify-content: flex-start; */
- /* align-items: center; */
- /* box-shadow: 1px 3px 12px 0px #0000007a; */
- /* border-radius: 10px; */
- /* padding: 1rem; */
- /* flex-direction: column; */
- /* @media (max-width:960px) {
+  /* height: 100%; */
+  width: 100%;
+  background: white;
+  padding: 3rem;
+  text-align: center;
+  /* justify-content: flex-start; */
+  /* align-items: center; */
+  /* box-shadow: 1px 3px 12px 0px #0000007a; */
+  /* border-radius: 10px; */
+  /* padding: 1rem; */
+  /* flex-direction: column; */
+  /* @media (max-width:960px) {
    padding: 2rem;
    width: 30rem;
    height: 80rem;
   } */
-  @media (max-width:500px) {
-  /* display: flex; */
-  /* margin: auto; */
-  /* margin-top: 1rem; */
-  /* width: 18rem; */
-  /* height: 105rem; */
-padding: 0 1rem;
-
-}
+  @media (max-width: 500px) {
+    /* display: flex; */
+    /* margin: auto; */
+    /* margin-top: 1rem; */
+    /* width: 18rem; */
+    /* height: 105rem; */
+    padding: 0 1rem;
+  }
 `;
 
 export const ChartsContainer = styled.div`
-margin:1rem 0;
-display: flex;
-padding: 0;
-gap:4rem;
-flex-direction:row;
-
-.Target-root{
-    margin-top:1rem;
-    font-family: 'Noto Kufi Arabic', sans-serif !important;
-  flex-direction: column-reverse;
-    }
-.css-4dretl.Target-root{
-  flex-direction: column-reverse;
+  margin: 1rem 0;
+  display: flex;
+  padding: 0;
+  gap: 4rem;
+  flex-direction: column;
+  .Target-root {
+    margin-top: 1rem;
+    font-family: "Noto Kufi Arabic", sans-serif !important;
+    flex-direction: column-reverse;
+  }
+  .css-4dretl.Target-root {
+    flex-direction: column-reverse;
   }
 
-@media(max-width:500px){
-  flex-direction: column-reverse;
-  gap:0rem;
-
-}
+  @media (max-width: 500px) {
+    flex-direction: column-reverse;
+    gap: 0rem;
+  }
 `;
-
 
 export const SelectInputContainer = styled.div`
 display: flex;
@@ -106,151 +102,145 @@ width:100%;
 }
 `;
 
-
 export const DivCenter = styled.div`
- box-shadow: 1px 3px 12px 0px #0000007a;
- margin: 1rem;
- margin-top: 2rem;
- width: 11rem;
- background: white ;
- border-radius: 10px;
- @media(max-width:1100px){
-  width: 8rem;
-  margin: .7rem;
-}
+  box-shadow: 1px 3px 12px 0px #0000007a;
+  margin: 1rem;
+  margin-top: 2rem;
+  width: 11rem;
+  background: white;
+  border-radius: 10px;
+  @media (max-width: 1100px) {
+    width: 8rem;
+    margin: 0.7rem;
+  }
 `;
-
 
 export const StatisticsContainer = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-
 export const Formm = styled.div`
-margin: auto;
-justify-content: center;
-align-items: flex-start;
-display: flex;
+  margin: auto;
+  justify-content: center;
+  align-items: flex-start;
+  display: flex;
 
-@media (max-width:900px) {
- /* display: flex; */
-flex-direction: column;
-}
+  @media (max-width: 900px) {
+    /* display: flex; */
+    flex-direction: column;
+  }
 
-@media(max-width:700px){ 
-  /* margin-right: -5rem; */
-  /* width: 30rem; */
-}
-
+  @media (max-width: 700px) {
+    /* margin-right: -5rem; */
+    /* width: 30rem; */
+  }
 `;
 
 export const DivPass = styled.div`
-&.green{
-  color: #088444;
-}
-&.red{
-  color: #eb2b2b;
-}
-width: 100%;
-padding: 0 5px;
-height: 40px;
-font-size: 16px;
-border: none;
-background: none;
-outline: none;
-color: gray;
-margin-top: .3rem;
-text-align: center;
+  &.green {
+    color: #088444;
+  }
+  &.red {
+    color: #eb2b2b;
+  }
+  width: 100%;
+  padding: 0 5px;
+  height: 40px;
+  font-size: 16px;
+  border: none;
+  background: none;
+  outline: none;
+  color: gray;
+  margin-top: 0.3rem;
+  text-align: center;
   /* :focus{
     border:0.2px solid #213C64;
   } */
 `;
 
 export const H1Login = styled.h1`
-/* margin-top: 1.5rem; */
-color: #2980b9 !important;
-font-weight: bold;
-text-align: center;
-padding: 2rem 0 10px 0;
-font-family: 'Noto Kufi Arabic', sans-serif;
-font-size: .9rem;
-border-bottom:0.1px solid silver;
-@media(max-width:1100px){
-  font-size: .6rem;
+  /* margin-top: 1.5rem; */
+  color: #2980b9 !important;
   font-weight: bold;
-}
+  text-align: center;
+  padding: 2rem 0 10px 0;
+  font-family: "Noto Kufi Arabic", sans-serif;
+  font-size: 0.9rem;
+  border-bottom: 0.1px solid silver;
+  @media (max-width: 1100px) {
+    font-size: 0.6rem;
+    font-weight: bold;
+  }
 `;
 
 export const Wird = styled.h1`
-text-align: center;
-/* padding: 0 0 20px 0; */
-font-family: 'Noto Kufi Arabic', sans-serif;
-font-size: 1.2rem;
-display: inline-block;
-color: #fff;
-padding: 1.3rem;
-width: 20rem;
-cursor: pointer;
-border-radius: 4px;
-background-color: #94C3D9;
-box-shadow: 1px 1px 13px 0px #4235357a;
+  text-align: center;
+  /* padding: 0 0 20px 0; */
+  font-family: "Noto Kufi Arabic", sans-serif;
+  font-size: 1.2rem;
+  display: inline-block;
+  color: #fff;
+  padding: 1.3rem;
+  width: 20rem;
+  cursor: pointer;
+  border-radius: 4px;
+  background-color: #94c3d9;
+  box-shadow: 1px 1px 13px 0px #4235357a;
 
+  @media (max-width: 1100px) {
+    font-size: 1rem;
+    font-weight: bold;
+  }
 
-@media(max-width:1100px){
-  font-size: 1rem;
-  font-weight: bold;
-}
-
-@media(max-width:500px){
-margin-top: 2rem;
-width: 15rem;
-
-}
+  @media (max-width: 500px) {
+    margin-top: 2rem;
+    width: 15rem;
+  }
 `;
 
 export const H3Login = styled.h3`
-text-align: center;
-/* margin-bottom: .5rem; */
-padding: 1rem 0 1rem 0;
-/* border-bottom: 1px solid silver; */
-font-family: 'Noto Kufi Arabic', sans-serif;
-font-size: 1rem;
-color: #E94f57 !important;
-@media(max-width:1100px){
-  font-size: .8rem;
-  font-weight: bold;
-}
+  text-align: center;
+  /* margin-bottom: .5rem; */
+  padding: 1rem 0 1rem 0;
+  /* border-bottom: 1px solid silver; */
+  font-family: "Noto Kufi Arabic", sans-serif;
+  font-size: 1rem;
+  color: #e94f57 !important;
+  @media (max-width: 1100px) {
+    font-size: 0.8rem;
+    font-weight: bold;
+  }
 `;
 
 export const DivTxtField = styled.div`
-position: relative;
-margin: 1rem 0;
+  position: relative;
+  margin: 1rem 0;
 `;
 
 export const FormInput = styled.input`
-width: 20rem;
-@media (max-width:550px) {
+  width: 20rem;
+  @media (max-width: 550px) {
     width: 100%;
-}
-padding: 0 5px;
-height: 2.1rem;
-font-size: 1rem;
-border: none;
-background: none;
-outline: none;
-text-align:right;
-color: orange;
-border-bottom: 2px solid #2691d9;
-:focus{
-transition: .1s;
-border-bottom: 3px solid orange;
-}
+  }
+  padding: 0 5px;
+  height: 2.1rem;
+  font-size: 1rem;
+  border: none;
+  background: none;
+  outline: none;
+  text-align: right;
+  color: orange;
+  border-bottom: 2px solid #2691d9;
+  :focus {
+    transition: 0.1s;
+    border-bottom: 3px solid orange;
+  }
 `;
 
 export const Span = styled.span`
-/* content: '';
+  /* content: '';
 position: absolute;
 top:2.1rem;
 left: 0;
@@ -268,25 +258,25 @@ transition: .1s;
 `;
 
 export const InputSubmit = styled.button`
-width:20rem;
-@media (max-width:550px) {
+  width: 20rem;
+  @media (max-width: 550px) {
     width: 100%;
-}
-height: 3.1rem;
-border: 1px;
-background: linear-gradient(120deg, #2980b9, #2980b9);
-border-radius: 1.5rem;
-font-size: 1rem;
-color: #e9f4fb;
-cursor: pointer;
-outline: none;
-margin-bottom: 1.5rem;
-margin-top: 2.2rem;
-:hover{
-background: orange;
-height: 3.2rem;
-transition: .3s;
-}
+  }
+  height: 3.1rem;
+  border: 1px;
+  background: linear-gradient(120deg, #2980b9, #2980b9);
+  border-radius: 1.5rem;
+  font-size: 1rem;
+  color: #e9f4fb;
+  cursor: pointer;
+  outline: none;
+  margin-bottom: 1.5rem;
+  margin-top: 2.2rem;
+  :hover {
+    background: orange;
+    height: 3.2rem;
+    transition: 0.3s;
+  }
 `;
 
 // export const FormLabel = styled.label`
@@ -304,62 +294,59 @@ transition: .3s;
 //   } */
 // `;
 
-
 export const DropdownDiv = styled.div`
-width: 20rem;
-@media (max-width:550px) {
+  width: 20rem;
+  @media (max-width: 550px) {
     width: 100%;
-}
-margin: auto;
-/* margin-bottom: 0rem; */
-margin-top: 1rem;
-@media (max-width:500px) {
-width: 15rem;
-margin-bottom: 1rem;
-margin-top: 1rem;
-}
+  }
+  margin: auto;
+  /* margin-bottom: 0rem; */
+  margin-top: 1rem;
+  @media (max-width: 500px) {
+    width: 15rem;
+    margin-bottom: 1rem;
+    margin-top: 1rem;
+  }
 `;
 
-
-
 export const DropdownList = styled.select`
-text-align: center;
-width: 20rem;
-@media (max-width:550px) {
+  text-align: center;
+  width: 20rem;
+  @media (max-width: 550px) {
     width: 100%;
-}
-font-family: 'Noto Kufi Arabic', sans-serif;
-/* color: #213C64; */
-color: white;
-font-size: 1.3rem;
-padding:1rem;
-border-radius: 4px;
-background-color: #E94F57;
-box-shadow: 1px 3px 12px 0px #0000007a;
-border: none;
-outline: none;
-:hover{
-  outline: none;
+  }
+  font-family: "Noto Kufi Arabic", sans-serif;
+  /* color: #213C64; */
+  color: white;
+  font-size: 1.3rem;
+  padding: 1rem;
+  border-radius: 4px;
+  background-color: #e94f57;
+  box-shadow: 1px 3px 12px 0px #0000007a;
   border: none;
-  color: #213C64;
-  background-color: white;
-}
-:focus{
   outline: none;
-  color: #213C64;
-  border: none;
-  background-color: white;
-}
-@media (max-width:500px) {
-  font-size: .8rem;
-  width: 15rem;
-}
+  :hover {
+    outline: none;
+    border: none;
+    color: #213c64;
+    background-color: white;
+  }
+  :focus {
+    outline: none;
+    color: #213c64;
+    border: none;
+    background-color: white;
+  }
+  @media (max-width: 500px) {
+    font-size: 0.8rem;
+    width: 15rem;
+  }
 `;
 
 export const DropdownListItem = styled.option`
-padding: 1rem ;
-text-align:center;
-/* :scope{
+  padding: 1rem;
+  text-align: center;
+  /* :scope{
   background-color: orange;
   color: white;
 }
@@ -367,7 +354,7 @@ text-align:center;
   background-color: orange;
   color: white;
 } */
-/* background-color: orange; */
+  /* background-color: orange; */
   &:hover {
     background-color: orange;
     color: white;
--- src/components/studentsPoints/TotalByLabelChart/index.jsx
+++ src/components/studentsPoints/TotalByLabelChart/index.jsx
@@ -1,57 +1,65 @@
-import React, { useState, useEffect } from 'react'
-import { retrieveStudentsPointsPerLabelORPerDay } from 'services/studentsServices';
+import React, { useState, useEffect } from "react";
+import { retrieveStudentsPointsPerLabelORPerDay } from "services/studentsServices";
 import {
-    Chart,
-    BarSeries,
-    Title,
-    ArgumentAxis,
-    ValueAxis,
-  } from '@devexpress/dx-react-chart-material-ui';
-  import { Animation } from '@devexpress/dx-react-chart';
-import {H5} from "../../Students/setPasswordStudent/SetPasswordStudent.styles";
+  Chart,
+  BarSeries,
+  Title,
+  ArgumentAxis,
+  ValueAxis,
+} from "@devexpress/dx-react-chart-material-ui";
+import { Animation } from "@devexpress/dx-react-chart";
+import { H5 } from "../../Students/setPasswordStudent/SetPasswordStudent.styles";
 
 export default function TotalByLabelChart({ selectedUser }) {
-    const [chartData, setChartData] = useState([]);
+  const [chartData, setChartData] = useState([]);
 
-
-    useEffect(() => {
-        if (selectedUser !== "") {
-            retrieveStudentsPointsPerLabelORPerDay(selectedUser, 'type', // retrieve result for the student per day
-                (res) => {
-                    setChartData(res.data.total_points_by_type)
-                },
-                (err) => {
-                    // *************** TODO: need to show message if no data within that day and that student ***************
-                    console.log("ERROR: " + JSON.stringify(err.response.data));
-                })
-        }else{
-            setChartData([]);
+  useEffect(() => {
+    if (selectedUser !== "") {
+      retrieveStudentsPointsPerLabelORPerDay(
+        selectedUser,
+        "type", // retrieve result for the student per day
+        (res) => {
+          setChartData(res.data.total_points_by_type);
+        },
+        (err) => {
+          // *************** TODO: need to show message if no data within that day and that student ***************
+          console.log("ERROR: " + JSON.stringify(err.response.data));
         }
-    }, [selectedUser]);
-
-    if(selectedUser === ""){
-        return <div style={{ width: '100%' }} className="table-msg-text-section"><H5>اختر الطالب لعرض الإحصائيات لكل معييار</H5></div>;
+      );
+    } else {
+      setChartData([]);
     }
+  }, [selectedUser]);
 
-    if(chartData.length ===0){
-        return <div style={{ width: '100%' }} className="table-msg-text-section"><H5>لا يوجد نقاط لعرض الإحصائيات لكل معييار</H5></div>;
-    }
-
+  if (selectedUser === "") {
     return (
-        <div style={{ width: '100%' }}>
-            <Chart
-              data={chartData}
-            >
-              <ArgumentAxis />
-              <ValueAxis max={1000} />
-
-              <BarSeries
-                valueField="total_point"
-                argumentField="point_template__label"
-              />
-              <Title text="الإحصائيات لكل معييار" />
-              <Animation />
-            </Chart>
+      <div style={{ width: "100%" }} className="table-msg-text-section">
+        <H5>اختر الطالب لعرض الإحصائيات لكل معييار</H5>
       </div>
-    )
+    );
+  }
+
+  if (chartData.length === 0) {
+    return (
+      <div style={{ width: "100%" }} className="table-msg-text-section">
+        <H5>لا يوجد نقاط لعرض الإحصائيات لكل معييار</H5>
+      </div>
+    );
+  }
+
+  return (
+    <div style={{ width: "auto" }}>
+      <Chart data={chartData} rotated>
+        <ArgumentAxis />
+        <ValueAxis max={1000} />
+
+        <BarSeries
+          valueField="total_point"
+          argumentField="point_template__label"
+        />
+        <Title text="الإحصائيات لكل معييار" />
+        <Animation />
+      </Chart>
+    </div>
+  );
 }
